0b4eb964ed98b64975da12ae7f23d4ce597a2a70
liubolif <lyubomyrif@gmail.com>
2021-02-10 22:17:57 +0200

    lab1 task3

--- lab_1/task_1/tcp_client.py
+++ lab_1/task_1/tcp_client.py
@@ -1,7 +1,7 @@
 import socket
 from datetime import datetime
 
-address = ('localhost', 44445)
+address = ('localhost', 44442)
 max_size = 1024
 
 print('Starting the CLIENT at', datetime.now())
@@ -10,8 +10,9 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 client.connect(address)
 
-client.sendall('Hey!'.encode('utf-8'))
-received_data = client.recv(max_size)
-
-print('At "{}" someone replied : "{}"'.format(datetime.now(), received_data.decode('utf-8')))
+message = input("Enter massage: ").strip()
+client.sendall(message.encode('utf-8'))
+if message != "q":
+    received_data = client.recv(max_size)
+    print('\tAt: {}\n\tsomeone replied: {}\n'.format(datetime.now(), received_data.decode('utf-8')))
 client.close()
--- lab_1/task_1/tcp_server.py
+++ lab_1/task_1/tcp_server.py
@@ -1,7 +1,8 @@
 import socket
 from datetime import datetime
+import time
 
-address = ('localhost', 44445)
+address = ('localhost', 44442)
 max_size = 1024
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -11,18 +12,21 @@ print('Starting the SERVER at', datetime.now())
 server.listen(5)
 print('Server is waiting for connection...')
 
+# тепер сервер здатен зчитувати інформацію не лише від одного клієнта
+# сервер отримує підключення від клієнта, слухає його, а опісля готовий до підключення наступного клієнту
+while True:
+    clientsocket, addr = server.accept()
+    print('Got a connection from client: {}'.format(addr))
+    received_data = clientsocket.recv(max_size)
 
-clientsocket, addr = server.accept()
-print('Got a connection from client: {}'.format(addr))
+    # якщо клієнт ввів ключовий символ 'q' - відбувається закриття з'єднання з клієнтом
+    if received_data.decode('utf-8') == 'q':
+        print('Quitting from the server')
+        break
 
-received_data = clientsocket.recv(max_size)
-print('At "{}" client "{}" said: "{}"'.format(datetime.now(), addr,  received_data.decode('utf-8')))
+    time.sleep(1)
+    print('\tAt: {} \n\tclient: {}\n\tsaid: {}\n'.format(datetime.now(), addr, received_data.decode('utf-8')))
 
-clientsocket.sendall('Dear client, are you talking to me?'.encode('utf-8'))
+    clientsocket.sendall('Dear client, are you talking to me?'.encode('utf-8'))
 
-clientsocket.close()
 server.close()
-
-
-
-
