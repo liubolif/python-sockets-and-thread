746296a6d9164875d531181b9917c62a82c2f7b8
liubolif <lyubomyrif@gmail.com>
2021-02-10 22:58:49 +0200

    lab1 task3_2 (where one client can send many massages)

--- lab_1/task_1/tcp_client.py
+++ lab_1/task_1/tcp_client.py
@@ -10,9 +10,15 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 client.connect(address)
 
-message = input("Enter massage: ").strip()
-client.sendall(message.encode('utf-8'))
-if message != "q":
-    received_data = client.recv(max_size)
-    print('\tAt: {}\n\tsomeone replied: {}\n'.format(datetime.now(), received_data.decode('utf-8')))
+print('Enter:\n\t\'q\' - to exit from client'
+      '\n\t\'qq\' - to shut down the server and client as well')
+
+while True:
+    message = input("Enter massage: ").strip()
+    client.sendall(message.encode('utf-8'))
+    if message != 'q' and message != 'qq':
+        received_data = client.recv(max_size)
+        print('\tAt: {}\n\tsomeone replied: {}\n'.format(datetime.now(), received_data.decode('utf-8')))
+    else:
+        break
 client.close()
--- lab_1/task_1/tcp_server.py
+++ lab_1/task_1/tcp_server.py
@@ -12,21 +12,31 @@ print('Starting the SERVER at', datetime.now())
 server.listen(5)
 print('Server is waiting for connection...')
 
+
 # тепер сервер здатен зчитувати інформацію не лише від одного клієнта
 # сервер отримує підключення від клієнта, слухає його, а опісля готовий до підключення наступного клієнту
-while True:
-    clientsocket, addr = server.accept()
-    print('Got a connection from client: {}'.format(addr))
-    received_data = clientsocket.recv(max_size)
 
-    # якщо клієнт ввів ключовий символ 'q' - відбувається закриття з'єднання з клієнтом
-    if received_data.decode('utf-8') == 'q':
-        print('Quitting from the server')
-        break
+class CloseServer(Exception): pass
 
-    time.sleep(1)
-    print('\tAt: {} \n\tclient: {}\n\tsaid: {}\n'.format(datetime.now(), addr, received_data.decode('utf-8')))
 
-    clientsocket.sendall('Dear client, are you talking to me?'.encode('utf-8'))
+try:
+    while True:
+        clientsocket, addr = server.accept()
+        print('Got a connection from client: {}'.format(addr))
+        while True:
 
-server.close()
+            received_data = clientsocket.recv(max_size)
+            # якщо клієнт ввів 'qq' - відбувається закриття з'єднання з клієнтом
+            if received_data.decode('utf-8') == 'qq':
+                print('Quitting from the server')
+                raise CloseServer
+            # якщо клієнт відімкнувся то виходмо із циклу обробки сповіщень від нього та готуємось до прослуховування нового клієнта
+            elif received_data.decode('utf-8') == 'q':
+                print('Client was disconnected from client'.format(addr))
+                break
+            time.sleep(1)
+            print('\tAt: {} \n\tclient: {}\n\tsaid: {}\n'.format(datetime.now(), addr, received_data.decode('utf-8')))
+
+            clientsocket.sendall('Dear client, are you talking to me?'.encode('utf-8'))
+except CloseServer:
+    server.close()
